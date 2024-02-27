b1a3b7d8853b7dd134e0344e3120eafb58b7a9f2
Aphichai Nasongkhram <6531503128@lamduan.mfu.ac.th>
2024-02-28 00:15:54 +0700

    matthew

--- lib/components/Details_page/head_details_section.dart
+++ lib/components/Details_page/head_details_section.dart
@@ -4,25 +4,15 @@ class HeadDetailSection extends StatelessWidget {
   const HeadDetailSection({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            expandedHeight: 300.0,
-            flexibleSpace: FlexibleSpaceBar(
-              centerTitle: true,
-              title: const Text('Weather Report'),
-              background: Image.asset(
-                'assets/images/unsplash.jpg',
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
+      body: SafeArea(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [ 
           
         ],
-      ),
-    );;
-
+      )),
+    );
   }
 }
--- lib/pages/details_place.dart
+++ lib/pages/details_place.dart
@@ -1,7 +1,6 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
-import 'package:myproject_travel/components/Home_page/heading.dart';
-import 'package:myproject_travel/components/Home_page/search.dart';
-import 'package:myproject_travel/components/Home_page/label_section_pop.dart';
 import 'package:myproject_travel/utils/styles.dart';
 
 class DatailPlace extends StatelessWidget {
@@ -9,12 +8,294 @@ class DatailPlace extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row( 
-      children: [ 
-        Text('2nd Page')
-      ],
-      
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width, // Ensure full width
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      alignment: Alignment.topCenter,
+                      height: MediaQuery.of(context).size.height / 3,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 224, 224, 224),
+                        image: DecorationImage(
+                          image: AssetImage('accest/images/wat.jpeg'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            InkWell(
+                              onTap: () {
+                                Navigator.pop(context);
+                              },
+                              child: Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                                child: Icon(
+                                  Icons.arrow_back,
+                                  size: 22,
+                                ),
+                              ),
+                            ),
+                            InkWell(
+                              onTap: () {
+                                Navigator.pop(context);
+                              },
+                              child: Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                                child: Icon(
+                                  Icons.favorite,
+                                  size: 22,
+                                  color: Colors.red,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(left: 20, top: 20),
+                      child: Row(
+                        children: [
+                          Text('Wat Rong Khun',
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold)),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Container(
+                            height: 30,
+                            width: 60,
+                            decoration: BoxDecoration(
+                              color: Colors.grey.shade300,
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Row(
+                                  children: [
+                                    Text('4.5'),
+                                    SizedBox(
+                                      width: 5,
+                                    ),
+                                    Icon(
+                                      Icons.star,
+                                      color: Colors.orange,
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(left: 20),
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 25,
+                            width: 67,
+                            decoration: BoxDecoration(
+                              color: Colors.orange,
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                'landmark',
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Container(
+                            height: 25,
+                            width: 60,
+                            decoration: BoxDecoration(
+                              color: Colors.orange,
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                'culture',
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    //Container for contain Gallery Section
+                    Container(
+                      padding: EdgeInsets.only(left: 20, top: 10, right: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Gallery',
+                            style: heading4,
+                          ),
+                          Icon(
+                            Icons.more_horiz,
+                            color: Colors.grey,
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(left: 20, right: 20, top: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            height: 100,
+                            width: 100,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              image: const DecorationImage(
+                                image: AssetImage('accest/images/wat.jpeg'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            height: 100,
+                            width: 100,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              image: const DecorationImage(
+                                image: AssetImage('accest/images/wat.jpeg'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            height: 100,
+                            width: 100,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              image: const DecorationImage(
+                                image: AssetImage('accest/images/wat.jpeg'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    //Map Section
+                    Container(
+                      padding: EdgeInsets.only(left: 20, top: 10, right: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'See the map',
+                            style: heading4,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(left: 20, top: 10, right: 20),
+                      child: Container(
+                        height: 200,
+                        width: 400, // Adjust this width as needed
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          image: const DecorationImage(
+                            image: AssetImage('accest/images/map.png'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(left: 20, top: 10),
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 25,
+                            width: 150,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                border: Border.all(color: accent)),
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                'open google map',
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Container(
+                            height: 25,
+                            width: 150,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                border: Border.all(color: accent)),
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                'contact with guide',
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    //Comment section
+                    Container(
+                      padding: EdgeInsets.only(left: 20, top: 10, right: 20),
+                      child: Row(
+                        children: [
+                          Text(
+                            'Comment',
+                            style: heading4,
+                          ),
+                          
+                        ],
+                      ),
+                    ),
+                    //Comment box
+                    Container(
+                      padding: EdgeInsets.only(left: 20, top: 10, right: 20),
+                      child: Column(
+                       
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
-             
   }
 }
